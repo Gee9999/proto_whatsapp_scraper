327ed0907ae0e10fbba0ecff00fa91f9b10dc5d9
Gee9999 <geozi1@gmail.com>
2025-12-06 21:19:25 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ st.title("💬 Proto WhatsApp Scraper")
 
 st.write("Click the button below to launch WhatsApp Web using Playwright.")
 
+# IMPORTANT: Do NOT run Playwright at import time!
+# Only trigger WhatsApp when button is clicked.
+
 if st.button("Open WhatsApp Web"):
     st.success("Launching WhatsApp…")
     open_whatsapp()
+else:
+    st.info("Waiting for user action…")
